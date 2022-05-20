ca8b293fe8744e6501436c4df662570aec558fce
huzaifaameer <79509748+huzaifaameer@users.noreply.github.com>
2022-05-20 20:22:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.post('/file/sow', (req, res) => {
                 .replace("{{Same date of SOW generation}}", date)
                 .replace("{{Terms 1}}", terms)
 
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({ args: [ '--no-sandbox', '--disable-setuid-sandbox' ] });
             const page = await browser.newPage();
             await page.setContent(html)
             await page.pdf({ path: file });
